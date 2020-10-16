f584a62eb7460929a00bd059fa40c1f35bd9b011
fabricioRG <fabricio21rg@gmail.com>
2020-10-16 10:28:36 -0600

    nuevos funcionalidades de metodos de envio

--- transporte.php
+++ transporte.php
@@ -26,6 +26,11 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/customStyles.css" rel="stylesheet">
+
+  <!-- Firebise -->
+  <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-firestore.js"></script>
 </head>
 
 <body>
@@ -44,8 +49,9 @@
             <ul>
               <li class="active"><a href="index.php">Home</a></li>
               <li class="active"><a href="#transporte">Nuevo Transporte</a></li>
-              <li class="active"><a href="#pedidos">Metodos de Envio </a></li>
-
+              <!-- <li class="active"><a href="#pedidos">Metodos de Envio </a></li> -->
+              <li class="active"><a href="#agregarMetodoEnvio">Agregar Metodo</a></li>
+              <li class="active"><a href="#zonaGeograficaEnvio">Zonas de Envío</a></li>
             </ul>
           </nav><!-- .nav-menu -->
         </div>
@@ -60,7 +66,7 @@
       <div class="row justify-content-center">
         <div class="col-xl-8">
           <h1>DROPSHOP - GESTION DE TRANSPORTE DE PEDIDOS</h1>
-  </div>
+        </div>
       </div>
     </div>
   </section><!-- End Hero -->
@@ -77,41 +83,42 @@
 
     <div class="container">
 
-        <div class="col-lg-8 mt-5 mt-lg-0">
+      <div class="col-lg-8 mt-5 mt-lg-0">
 
-          <form method="post">
-            <div class="form-row">
-              <div class="col-md-6 form-group">
-                <div class="section-title">
-                  <p>Nombre</p>
-                </div>
-                <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Nombre" data-rule="minlen:6" data-msg="Porfavor ingresa un nombre valido." />
-                <div class="validate"></div>
+        <form method="post" role="form" autocomplete="off" class="php-email-form">
+          <div class="form-row">
+            <div class="col-md-6 form-group">
+              <div class="section-title">
+                <p>Nombre</p>
               </div>
-              <div class="col-md-6 form-group">
-                <div class="section-title">
-                  <p>Precio</p>
-                </div>
-                <input type="number" min=0.05 step="1.0" name="precio" class="form-control" id="precio" placeholder="Precio del Produto" data-rule-number data-msg="Porfavor ingresa un precio valido" />
-                <div class="validate"></div>
+              <input type="text" name="nombre" class="form-control" id="nameTransport" placeholder="Nombre" data-msg="Porfavor ingresa un nombre valido." />
+              <div class="validate" id="validateNameTransport"></div>
+            </div>
+            <div class="col-md-6 form-group">
+              <div class="section-title">
+                <p>Descripcion</p>
               </div>
+              <input type="text" name="nombre" class="form-control" id="descriptionTransport" placeholder="Descripcion" data-msg="Porfavor ingresa una descripcion valida." />
+              <div class="validate" id="validateDescriptionTransport"></div>
+            </div>
 
-            <div class="text-center" >
-              <button type="submit"  >
-                Ingresar
+            <div class="text-center">
+              <button type="submit" onclick="saveTransportista()">
+                Agregar
               </button>
             </div>
-          </form>
-
-        </div>
+          </div>
+        </form>
 
       </div>
+
+    </div>
 
     </div>
   </section><!-- End Sesion Section -->
 
   <!-- ======= Sesion Section ======= -->
-  <section id="pedidos" class="contact">
+  <!-- <section id="pedidos" class="contact">
     <div class="container">
 
       <div class="section-title">
@@ -121,29 +128,151 @@
 
     <div class="container">
 
-        <div class="col-lg-8 mt-5 mt-lg-0">
+      <div class="col-lg-8 mt-5 mt-lg-0">
 
-          <form method="post"  role="form" class="php-email-form">
-            <div class="form-row">
-              <div class="col-md-6 form-group">
-                <div class="section-title">
-                  <p>Servicio Numero 1           Q. 125.00 <input type="checkbox" id="cbox1" value="second_checkbox"></p>
-                  <p>Servicio Numero 2           Q. 115.00 <input type="checkbox" id="cbox2" value="second_checkbox"></p>
-                  <p>Servicio Numero 3           Q. 135.00 <input type="checkbox" id="cbox3" value="second_checkbox"></p>
-                  <p>Servicio Numero 4           Q. 120.00 <input type="checkbox" id="cbox4" value="second_checkbox"></p>
-                </div>
-                <div class="text-center"><button type="submit">Validar Transporte</button></div>
+        <form method="post" role="form" class="php-email-form">
+          <div class="form-row">
+            <div class="col-md-6 form-group">
+              <div class="section-title">
+                <p>Servicio Numero 1 Q. 125.00 <input type="checkbox" id="cbox1" value="second_checkbox"></p>
+                <p>Servicio Numero 2 Q. 115.00 <input type="checkbox" id="cbox2" value="second_checkbox"></p>
+                <p>Servicio Numero 3 Q. 135.00 <input type="checkbox" id="cbox3" value="second_checkbox"></p>
+                <p>Servicio Numero 4 Q. 120.00 <input type="checkbox" id="cbox4" value="second_checkbox"></p>
               </div>
+              <div class="text-center"><button type="submit">Validar Transporte</button></div>
+            </div>
 
-          </form>
-
-        </div>
+        </form>
 
       </div>
 
     </div>
-  </section><!-- End Sesion Section -->
 
+    </div>
+  </section> -->
+  <!-- End Sesion Section -->
+
+  <section id="agregarMetodoEnvio" class="contact">
+
+    <div class="container">
+      <div class="section-title">
+        <h2>Agregar nuevo método de envio</h2>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <form method="post" role="form" autocomplete="off" class="php-email-form">
+        <div class="form-row">
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Transporte</label>
+            <select class="form-control custom-select" id="nombreTransporte"></select>
+          </div>
+          <div class="form-group input-field">
+            <label>Nombre</label>
+            <input type="text" name="nombreMetodoEnvio" class="form-control" id="nombreMetodoEnvio" placeholder="Nombre del método" data-rule="minlen:6" data-msg="Porfavor ingresa un nombre valido." />
+          </div>
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Descripción</label>
+            <input type="text" name="descripcionMetodoEnvio" class="form-control" id="descripcionMetodoEnvio" placeholder="Descripción del método" data-rule="minlen:6" data-msg="Porfavor ingresa un nombre valido." />
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Horario disponible</label>
+            <div class="form-row">
+              <div class="form-group input-time-medium">
+                <input class="form-control" id="inicioHorario" type="time" name="appt-time" value="09:00">
+              </div>
+              <div class="form-group label-margin">
+                <label>a</label>
+              </div>
+              <div class="form-group input-time-medium">
+                <input class="form-control" id="finHorario" type="time" name="appt-time" value="18:00">
+              </div>
+            </div>
+          </div>
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Disponibilidad</label>
+            <div class="dowPicker">
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow1">
+                <label class="labelPicker" onclick="setStateToDay('sunday')" for="dow1">D</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow2">
+                <label class="labelPicker" onclick="setStateToDay('monday')" for="dow2">L</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow3">
+                <label class="labelPicker" onclick="setStateToDay('tuesday')" for="dow3">M</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow4">
+                <label class="labelPicker" onclick="setStateToDay('wednesday')" for="dow4">M</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow5">
+                <label class="labelPicker" onclick="setStateToDay('thursday')" for="dow5">J</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow6">
+                <label class="labelPicker" onclick="setStateToDay('friday')" for="dow6">V</label>
+              </div>
+              <div class="dowPickerOption">
+                <input type="checkbox" id="dow7">
+                <label class="labelPicker" onclick="setStateToDay('saturday')" for="dow7">S</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="text-center">
+          <button type="submit" id="botonP" onclick="saveMetodoEnvio()">Agregar Método</button>
+        </div>
+      </form>
+
+    </div>
+
+  </section>
+
+  <section id="zonaGeograficaEnvio" class="contact">
+
+    <div class="container">
+      <div class="section-title">
+        <h2>Zonas geógraficas de envío</h2>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <form method="post" role="form" autocomplete="off" class="php-email-form">
+        <div class="form-row">
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Metodo de Envío</label>
+            <select class="form-control custom-select" id="nombresMetodoEnvio"></select>
+          </div>
+          <div class="form-group input-field">
+            <label>Pais</label>
+            <select class="form-control custom-select" id="nombresPaises"></select>
+          </div>
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Ciudad</label>
+            <select class="form-control custom-select" id="nombresCiudades"></select>
+          </div>
+          <div class="form-group input-field">
+            <label for="exampleInputEmail1">Costo</label>
+            <input type="number" min=0.0 step="1.0" name="precio" class="form-control" id="precioZona" placeholder="Precio del Produto" data-rule-number data-msg="Porfavor ingresa un precio valido" />
+          </div>
+        </div>
+        <div class="text-center">
+          <button type="submit" id="botonP" onclick="saveZonaEnvio()">Agregar Nueva Zona</button>
+        </div>
+      </form>
+
+    </div>
+
+  </section>
 
   </main><!-- End #main -->
 
@@ -187,7 +316,8 @@
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
-  <script src="Registro.js"></script>
+  <script type="text/javascript" src="Registro.js"></script>
+  <script type="text/javascript" src="assets/js/registrarEnvio.js"></script>
 </body>
 
 </html>
